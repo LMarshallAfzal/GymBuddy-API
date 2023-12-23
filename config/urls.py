6c2b786cb3dd104935798e68b7e1030f9c7a54e9
Leonard Marshall Afzal <loma256@gmail.com>
2023-12-23 02:26:08 +0000

    Renambe gymbuddy directory to config

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from exercises.views import UserViewSet, GroupViewSet
+from api.views import UserViewSet, GroupViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
