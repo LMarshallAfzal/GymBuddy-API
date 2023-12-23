540995e5fbe296913d141cc1642b6a5b0083776f
Leonard Marshall Afzal <loma256@gmail.com>
2023-12-23 21:59:09 +0000

    Implement seed and unseed management commands
    - Create seed command that populates the db with values from the dataset
    - Create unseed command removes all data from db

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=255, blank=False)
-    description = models.TextField()
-    type = models.CharField(max_length=25)
-    muscle_group = models.CharField(max_length=30)
-    equipment = models.CharField(max_length=15)
-    level = models.CharField(max_length=15)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=25, blank=True, null=True)
+    muscle_group = models.CharField(max_length=30, blank=True, null=True)
+    equipment = models.CharField(max_length=15, blank=True, null=True)
+    level = models.CharField(max_length=15, blank=True, null=True)
 
 class Image(models.Model):
     url = models.CharField(max_length=500)
