ee50172fe3fbb11329370bc18479a3c8422b1921
Leonard Marshall Afzal <loma256@gmail.com>
2023-12-26 03:18:25 +0000

    Update model tests & add view tests
    - Update exercise model tests witth tests for new image field
    - Delete image model tests because image model is gone
    - Create excerse view tests for retrieving all exercises
    - Add fixtures for view tests

--- api/views.py
+++ api/views.py
@@ -1,7 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework import status
 from api.serializers import ExerciseSerializer
 from api.models import Exercise
 
