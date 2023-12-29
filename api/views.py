9535e8c8b4b514b1ef316e53713e8c88002751f3
Leonard Marshall Afzal <loma256@gmail.com>
2023-12-29 02:33:40 +0000

    Remove core api package

--- api/views.py
+++ api/views.py
@@ -1,8 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.decorators import permission_classes, api_view
-from rest_framework.permissions import AllowAny
-from rest_framework.schemas import get_schema_view
+from rest_framework.decorators import api_view
 from api.serializers import ExerciseSerializer
 from api.models import Exercise
 
@@ -16,6 +14,7 @@ class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.get_all_exercises()
     serializer_class = ExerciseSerializer
 
+    @api_view(["GET"])
     def list(self, request):
         """
         Retrieve a list of exercises.
