2d364a51c06b1cb035085fd26c82551ce6ea7b4e
Leonard Marshall Afzal <loma256@gmail.com>
2023-12-30 23:30:48 +0000

    Add docstrings to exercise serializer

--- api/serializers.py
+++ api/serializers.py
@@ -4,22 +4,62 @@ from .helpers import validation_helpers
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Exercise models, handling validation and serialization of exercise data.
+    """
     class Meta:
         model = Exercise
         fields = "__all__"
 
     def validate_name(self, value):
+        """
+        Validates the exercise name.
+
+        Args:
+            value (str): The provided name value.
+
+        Raises:
+            serializers.ValidationError: If the name is empty, exceeds 100 characters, or is invalid.
+
+        Returns:
+            str: The validated name value.
+        """
         validation_helpers._validate_required_and_max_length(value, "Name", 100)
         return value
     
     def validate_image_urls(self, value):
-        """Validates all image URLs."""
+        """
+        Validates all image URLs.
+
+        Args:
+            value (dict): The dictionary containing image URL fields.
+
+        Raises:
+            serializers.ValidationError: If any of the image URLs are invalid.
+
+        Returns:
+            dict: The validated dictionary of image URLs.
+        """
         image_fields = ["image1", "image2", "image3", "image4"]
         for field_name in image_fields:
             validation_helpers._validate_image_url(value.get(field_name), field_name)
         return value
     
     def validate(self, data):
+        """
+        Performs overall validation of exercise data, including:
+        - Validating muscle group, exercise type, and equipment against allowed values.
+        - Ensuring at least one image URL is provided.
+
+        Args:
+            data (dict): The complete exercise data to be validated.
+
+        Raises:
+            serializers.ValidationError: If any validation rules are violated.
+
+        Returns:
+            dict: The validated exercise data.
+        """
         super().validate(data)
 
         validation_helpers._validate_choices(data, "muscle_group", valid_values=[
