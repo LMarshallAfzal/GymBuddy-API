66b55406a18f9449fdf34e4587f02321e887a488
Leonard Marshall Afzal <loma256@gmail.com>
2023-12-31 22:39:21 +0000

    Rename config directory to gymbuddy

--- gymbuddy/settings.py
+++ gymbuddy/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'gymbuddy.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = 'gymbuddy.wsgi.application'
 
 
 # Database
