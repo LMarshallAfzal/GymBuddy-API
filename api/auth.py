f98908501bf25c06c4465428c8979c4fa98d7ce3
Leonard Marshall Afzal <loma256@gmail.com>
2024-01-08 23:39:54 +0000

    Update auth.py to use API key in AWS Secret Manager
    - Add get_secret function  to APIKeyAuthentication class
    - Update authenticate function comparison against correct AWS secret

--- api/auth.py
+++ api/auth.py
@@ -1,5 +1,8 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+import boto3
+import json
+from botocore.exceptions import NoCredentialsError
 from decouple import config
 
 class ApiKeyAuthentication(BaseAuthentication):
@@ -36,7 +39,38 @@ class ApiKeyAuthentication(BaseAuthentication):
         elif api_key is None:
             raise AuthenticationFailed("No valid API key present")
         
-        elif api_key == config('API_KEY'):
-            return (None, None)
-        else:
-            raise AuthenticationFailed("Invalid API Key")
+        # elif api_key == config('API_KEY'):
+        #     return (None, None)
+        # else:
+        #     raise AuthenticationFailed("Invalid API Key")
+        
+        try: 
+            secret_name = "prod/gymbuddy/api-key"
+            secret_value = self.get_secret(secret_name)
+
+
+            if api_key == secret_value:
+                return (None, None)
+            else:
+                print(secret_value)
+                raise AuthenticationFailed("Invalid API Key")
+            
+        except NoCredentialsError:   
+            raise AuthenticationFailed("Unable to authenticate due to missing AWS credentials")
+        
+    def get_secret(self, secret_name):
+        client = boto3.client(
+            service_name='secretsmanager',
+            region_name='eu-west-2'
+        )
+
+        try:
+            response =  client.get_secret_value(SecretId=secret_name)
+            secret_value = response['SecretString']
+            secret_dict = json.loads(secret_value)
+            api_key_value = secret_dict.get("API_Key")
+
+            return api_key_value
+        
+        except Exception as e:
+            raise AuthenticationFailed(f"Error retrieving secret from AWS Secrets Manager: {e}")
