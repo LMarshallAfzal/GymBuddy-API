d9844d26fca6c9ed00c895e5d5d8fba433686d4e
Leonard Marshall Afzal <loma256@gmail.com>
2024-01-14 00:35:18 +0000

    Update max_length validator for description

--- api/models.py
+++ api/models.py
@@ -18,7 +18,7 @@ class Exercise(models.Model):
     """
 
     name = models.CharField(unique=True, max_length=255, blank=False, null=False)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(max_length=1200, blank=True, null=True)
     type = models.CharField(max_length=25, blank=True, null=True)
     muscle_group = models.CharField(max_length=30, blank=True, null=True)
     equipment = models.CharField(max_length=15, blank=True, null=True)
